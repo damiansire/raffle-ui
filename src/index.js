4f3310938a4cd42930ba992baf7d7f9efab5ca02
Damian Sire <damiansire@gmail.com>
2025-04-19 04:34:11 +0700

    refactor: replace index.js with index.ts for component exports and update imports

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './components/Button/Button.js';
-import './components/Header/Header.js';
-import './components/Page/Page.js';
-
-export { RaffleButton } from './components/Button/Button.js';
-export { RaffleHeader } from './components/Header/Header.js';
-export { RafflePage } from './components/Page/Page.js'; 
