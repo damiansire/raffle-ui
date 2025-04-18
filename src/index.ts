d67f06a6e7faa83f113aa82cf9438ef334b90dc2
Damian Sire <damiansire@gmail.com>
2025-04-19 04:34:11 +0700

    refactor: replace index.js with index.ts for component exports and update imports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Import component definitions to register custom elements
+import './components/ContributionGrid/ContributionGrid';
+import './components/WinnerModal/WinnerModal';
+
+// Export component classes for potential direct use or type checking
+export { ContributionGrid } from './components/ContributionGrid/ContributionGrid';
+export { WinnerModal } from './components/WinnerModal/WinnerModal'; 
